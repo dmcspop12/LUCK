d8b49b43411f5080e3be0f6005576838e5af5177
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-24 01:46:03 +0800

    refactor

--- src/mods/sample_mod/main.py
+++ src/mods/sample_mod/main.py
@@ -26,24 +26,58 @@ def get_data_by_prefix(asset_env: UnityPy.Environment, prefix: str):
     return None
 
 
-def debug_dump(character_table):
+def debug_dump(table, prefix: str):
     with open(
         Path(
             TMP_DIRPATH,
-            "sample_mod.json",
+            f"sample_mod_{prefix}.json",
         ),
         "w",
         encoding="utf-8",
     ) as f:
         json.dump(
-            character_table,
+            table,
             f,
             ensure_ascii=False,
             indent=4,
         )
 
 
-def sample_mod(character_table):
+def mod_table(res: Resource, prefix: str, do_mod_func):
+    table_ab_name = None
+    for anon_asset_name in res.anon_asset_name_dict:
+        if anon_asset_name.startswith(prefix):
+            table_ab_name = res.anon_asset_name_dict[anon_asset_name]
+            break
+
+    if table_ab_name is None:
+        raise FileNotFoundError(f"{prefix} not found")
+
+    table_asset_env = res.get_asset_env(table_ab_name)
+
+    res.mark_modified_asset(table_ab_name)
+
+    data = get_data_by_prefix(table_asset_env, prefix)
+
+    script_bytes = remove_header(script_to_bytes(data.m_Script))
+
+    table_str = decode_flatc(script_bytes, res.client_version, prefix)
+    table = json.loads(table_str)
+
+    do_mod_func(table)
+
+    debug_dump(table, prefix)
+
+    new_table_str = json.dumps(table)
+
+    new_script_bytes = encode_flatc(new_table_str, res.client_version, prefix)
+
+    data.m_Script = bytes_to_script(add_header(new_script_bytes))
+
+    data.save()
+
+
+def do_mod_character_table(character_table):
     for char in character_table["characters"]:
         if char["key"] == "char_1035_wisdel":
             data = char["value"]["phases"][-1]["attributesKeyFrames"][-1]["data"]
@@ -57,43 +91,7 @@ def main():
 
     res.load_anon_asset()
 
-    character_table_prefix = "character_table"
-
-    character_table_ab_name = None
-    for anon_asset_name in res.anon_asset_name_dict:
-        if anon_asset_name.startswith(character_table_prefix):
-            character_table_ab_name = res.anon_asset_name_dict[anon_asset_name]
-            break
-
-    if character_table_ab_name is None:
-        raise FileNotFoundError(f"{character_table_prefix} not found")
-
-    character_table_asset_env = res.get_asset_env(character_table_ab_name)
-
-    res.mark_modified_asset(character_table_ab_name)
-
-    data = get_data_by_prefix(character_table_asset_env, character_table_prefix)
-
-    script_bytes = remove_header(script_to_bytes(data.m_Script))
-
-    character_table_str = decode_flatc(
-        script_bytes, res.client_version, character_table_prefix
-    )
-    character_table = json.loads(character_table_str)
-
-    sample_mod(character_table)
-
-    debug_dump(character_table)
-
-    new_character_table_str = json.dumps(character_table)
-
-    new_script_bytes = encode_flatc(
-        new_character_table_str, res.client_version, character_table_prefix
-    )
-
-    data.m_Script = bytes_to_script(add_header(new_script_bytes))
-
-    data.save()
+    mod_table(res, "character_table", do_mod_character_table)
 
     res.build_mod("sample_mod")
 

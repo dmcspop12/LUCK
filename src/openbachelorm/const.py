19a0cd0020baa837e6d038cf90f8254c1828db13
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-25 21:25:29 +0800

    known table

--- src/openbachelorm/const.py
+++ src/openbachelorm/const.py
@@ -1,5 +1,13 @@
+from enum import StrEnum
+
 TMP_DIRPATH = "tmp/"
 
 ASSET_DIRPATH = "asset/"
 
 MOD_DIRPATH = "mod/"
+
+
+class KnownTable(StrEnum):
+    CHARACTER_TABLE = "character_table"
+    SKILL_TABLE = "skill_table"
+    RANGE_TABLE = "range_table"

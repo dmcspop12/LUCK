0861cf963f2ef1b33ad95964a7f471b681319fea
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-25 22:11:55 +0800

    loop

--- test/test_known_table.py
+++ test/test_known_table.py
@@ -8,27 +8,12 @@ def load_known_table(client_version: str, res_version: str):
 
     res.load_anon_asset()
 
-    res.mod_table(
-        KnownTable.CHARACTER_TABLE.value,
-        nop_mod_table_func,
-        get_known_table_decorator_lst(
-            KnownTable.CHARACTER_TABLE, client_version, res_version
-        ),
-    )
-    res.mod_table(
-        KnownTable.SKILL_TABLE.value,
-        nop_mod_table_func,
-        get_known_table_decorator_lst(
-            KnownTable.SKILL_TABLE, client_version, res_version
-        ),
-    )
-    res.mod_table(
-        KnownTable.RANGE_TABLE.value,
-        nop_mod_table_func,
-        get_known_table_decorator_lst(
-            KnownTable.RANGE_TABLE, client_version, res_version
-        ),
-    )
+    for known_table in KnownTable:
+        res.mod_table(
+            known_table.value,
+            nop_mod_table_func,
+            get_known_table_decorator_lst(known_table, client_version, res_version),
+        )
 
 
 def test_known_table():

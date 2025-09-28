8842ec20c647018969347667ce35ea51dd929174
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-29 02:06:58 +0800

    bundle list

--- src/openbachelorm/manifest.py
+++ src/openbachelorm/manifest.py
@@ -1,4 +1,15 @@
+from dataclasses import dataclass
+from copy import deepcopy
+
 from .resource import Resource
+
+
+@dataclass
+class ManifestBundle:
+    name: str
+    props: int
+    sccIndex: int
+    allDependencies: list[int]
 
 
 class ManifestManager:
@@ -8,3 +19,18 @@ class ManifestManager:
         res.load_manifest()
 
         self.manifest = res.manifest
+
+        self.build_bundle_lst()
+
+    def build_bundle_lst(self):
+        self.bundle_lst = []
+
+        for bundle_dict in self.manifest["bundles"]:
+            bundle = ManifestBundle(
+                name=bundle_dict.get("name"),
+                props=bundle_dict.get("props", 0),
+                sccIndex=bundle_dict.get("sccIndex", 0),
+                allDependencies=deepcopy(bundle_dict.get("allDependencies")),
+            )
+
+            self.bundle_lst.append(bundle)

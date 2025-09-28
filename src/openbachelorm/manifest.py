908b7da67db44bf7d492717d13712581efff488e
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-29 02:32:27 +0800

    asset

--- src/openbachelorm/manifest.py
+++ src/openbachelorm/manifest.py
@@ -14,6 +14,14 @@ class ManifestBundle:
     dep_on_lst: list["ManifestBundle"] = field(default_factory=list)
 
 
+@dataclass
+class ManifestAsset:
+    assetName: str
+    bundleIndex: int
+    name: str
+    path: str
+
+
 class ManifestManager:
     def __init__(self, res: Resource):
         self.resource = res
@@ -23,6 +31,7 @@ class ManifestManager:
         self.manifest = res.manifest
 
         self.build_bundle_lst()
+        self.build_asset_tree()
 
     def build_bundle_lst(self):
         self.bundle_lst: list[ManifestBundle] = []
@@ -43,3 +52,12 @@ class ManifestManager:
 
             for i in bundle.allDependencies:
                 bundle.dep_on_lst.append(self.bundle_lst[i])
+
+    def build_asset_tree(self):
+        for asset_dict in self.manifest["assetToBundleList"]:
+            asset = ManifestAsset(
+                assetName=asset_dict.get("assetName"),
+                bundleIndex=asset_dict.get("bundleIndex", 0),
+                name=asset_dict.get("name"),
+                path=asset_dict.get("path"),
+            )

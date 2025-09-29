1998635d952abd917ebd3662e89902d75848bf50
pfyy <112098515+pfyy@users.noreply.github.com>
2025-09-29 10:06:54 +0800

    efficient node lookup

--- src/openbachelorm/manifest.py
+++ src/openbachelorm/manifest.py
@@ -27,14 +27,33 @@ class ManifestAsset:
     bundle: "ManifestBundle" = None
 
 
-def create_child_node_if_necessary(node: Node, child_name: str) -> Node:
-    for child in node.children:
-        if child.name == child_name:
-            if not child.is_dir:
-                print(f"warn: file {child} used as folder")
-            return child
+def add_node_to_parent(parent: Node, name: str, node: Node):
+    if parent is not None:
+        if name in parent.child_dict:
+            raise KeyError(f"{node} already exist")
+        parent.child_dict[name] = node
 
-    child = Node(child_name, parent=node, is_dir=True)
+
+def new_dir_node(dir_name: str, parent: Node = None) -> Node:
+    node = Node(dir_name, parent=parent, is_dir=True, child_dict={})
+    add_node_to_parent(parent, dir_name, node)
+    return node
+
+
+def new_file_node(file_name: str, asset: ManifestAsset, parent: Node = None):
+    node = Node(file_name, parent=parent, is_dir=False, asset=asset)
+    add_node_to_parent(parent, file_name, node)
+    return node
+
+
+def create_child_node_if_necessary(node: Node, child_name: str) -> Node:
+    if not node.is_dir:
+        raise KeyError(f"{node} not a dir")
+
+    if child_name not in node.child_dict:
+        child = new_dir_node(child_name, node)
+    else:
+        child = node.child_dict[child_name]
 
     return child
 
@@ -81,16 +100,12 @@ class ManifestManager:
         for dir_name in asset_path.parent.parts:
             cur_node = create_child_node_if_necessary(cur_node, dir_name)
 
-        for child in cur_node.children:
-            if child.name == asset_path.name:
-                print(f"warn: {asset_path} already added")
-
-        asset_node = Node(asset_path.name, parent=cur_node, is_dir=False, asset=asset)
+        asset_node = new_file_node(asset_path.name, asset, cur_node)
 
         return asset_node
 
     def build_asset_tree(self):
-        self.asset_tree_root = Node(ASSET_TREE_ROOT_NAME, is_dir=True)
+        self.asset_tree_root = new_dir_node(ASSET_TREE_ROOT_NAME)
         self.dangling_asset_lst: list[ManifestAsset] = []
 
         for asset_dict in self.manifest["assetToBundleList"]:

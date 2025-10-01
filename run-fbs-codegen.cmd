43b230ab1f25026783a4f4b86817b4da1b8f9409
pfyy <112098515+pfyy@users.noreply.github.com>
2025-10-01 20:29:39 +0800

    workaround

--- run-fbs-codegen.cmd
+++ run-fbs-codegen.cmd
@@ -1,8 +1,8 @@
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_41/ fbs/2.6.41/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_21/ fbs/2.6.21/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_01/ fbs/2.6.01/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_80/ fbs/2.5.80/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_60/ fbs/2.5.60/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_04/ fbs/2.5.04/prts___levels.fbs
-flatc --python --gen-object-api -o src/openbachelorm/fbs_codegen/v2_4_61/ fbs/2.4.61/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_41/ fbs/2.6.41/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_21/ fbs/2.6.21/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_6_01/ fbs/2.6.01/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_80/ fbs/2.5.80/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_60/ fbs/2.5.60/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_5_04/ fbs/2.5.04/prts___levels.fbs
+flatc --python --gen-onefile --gen-object-api -o src/openbachelorm/fbs_codegen/v2_4_61/ fbs/2.4.61/prts___levels.fbs
 pause

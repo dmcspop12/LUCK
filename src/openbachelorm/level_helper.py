e00c256ebafa0172a56d4a145d42eed8f1064de2
pfyy <112098515+pfyy@users.noreply.github.com>
2025-10-01 20:31:28 +0800

    fix

--- src/openbachelorm/level_helper.py
+++ src/openbachelorm/level_helper.py
@@ -1,48 +1,48 @@
 from .fbs_codegen.v2_6_41 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_6_41,
+    prts___levels_generated as prts___levels_v2_6_41,
 )
 from .fbs_codegen.v2_6_21 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_6_21,
+    prts___levels_generated as prts___levels_v2_6_21,
 )
 from .fbs_codegen.v2_6_01 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_6_01,
+    prts___levels_generated as prts___levels_v2_6_01,
 )
 from .fbs_codegen.v2_5_80 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_5_80,
+    prts___levels_generated as prts___levels_v2_5_80,
 )
 from .fbs_codegen.v2_5_60 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_5_60,
+    prts___levels_generated as prts___levels_v2_5_60,
 )
 from .fbs_codegen.v2_5_04 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_5_04,
+    prts___levels_generated as prts___levels_v2_5_04,
 )
 from .fbs_codegen.v2_4_61 import (
-    clz_Torappu_LevelData as clz_Torappu_LevelData_v2_4_61,
+    prts___levels_generated as prts___levels_v2_4_61,
 )
 
 
-def get_clz_Torappu_LevelData(client_version: str):
+def get_prts___levels(client_version: str):
     match client_version:
         case "2.6.41":
-            return clz_Torappu_LevelData_v2_6_41
+            return prts___levels_v2_6_41
 
         case "2.6.21":
-            return clz_Torappu_LevelData_v2_6_21
+            return prts___levels_v2_6_21
 
         case "2.6.01":
-            return clz_Torappu_LevelData_v2_6_01
+            return prts___levels_v2_6_01
 
         case "2.5.80":
-            return clz_Torappu_LevelData_v2_5_80
+            return prts___levels_v2_5_80
 
         case "2.5.60":
-            return clz_Torappu_LevelData_v2_5_60
+            return prts___levels_v2_5_60
 
         case "2.5.04":
-            return clz_Torappu_LevelData_v2_5_04
+            return prts___levels_v2_5_04
 
         case "2.4.61":
-            return clz_Torappu_LevelData_v2_4_61
+            return prts___levels_v2_4_61
 
         case _:
             raise ValueError(f"fbs codegen not found for {client_version}")
